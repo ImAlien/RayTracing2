dcba2c0652ece2b3e687779163adf3c3a9d50a0e
ImAlien <577068566@qq.com>
2023-03-02 15:44:30 +0800

    add microfacet, brute sample

--- src/microfacet.cpp
+++ src/microfacet.cpp
@@ -38,23 +38,58 @@ public:
 
     /// Evaluate the BRDF for the given pair of directions
     Color3f eval(const BSDFQueryRecord &bRec) const {
-    	throw NoriException("MicrofacetBRDF::eval(): not implemented!");
+    	//throw NoriException("MicrofacetBRDF::eval(): not implemented!");
+        Vector3f wh = (bRec.wi + bRec.wo).normalized();
+        auto cosThetaI = Frame::cosTheta(bRec.wi);
+        auto cosThetaO = Frame::cosTheta(bRec.wo);
+        auto d = DistributeBeckmann(wh, m_alpha);
+        auto g = G1(bRec.wi, wh, m_alpha) * G1(bRec.wo, wh, m_alpha);
+        auto f = fresnel(wh.dot(bRec.wi), m_extIOR, m_intIOR);
+        //kd + ks
+        return m_kd / M_PI + m_ks * ((d * f * g) / (4.0f * cosThetaI * cosThetaO));
     }
 
     /// Evaluate the sampling density of \ref sample() wrt. solid angles
     float pdf(const BSDFQueryRecord &bRec) const {
-    	throw NoriException("MicrofacetBRDF::pdf(): not implemented!");
+    	//throw NoriException("MicrofacetBRDF::pdf(): not implemented!");
+        if (bRec.wo.z() <= 0) {
+            return 0;
+        }
+        Vector3f wh = (bRec.wi + bRec.wo).normalized();
+        float d = DistributeBeckmann(wh, m_alpha);
+        float jacobian = 1 / (4.0f * abs(wh.dot(bRec.wo)));
+        return m_ks * d  * Frame::cosTheta(wh) * jacobian + (1 - m_ks) * Frame::cosTheta(bRec.wo) * INV_PI;
     }
 
     /// Sample the BRDF
     Color3f sample(BSDFQueryRecord &bRec, const Point2f &_sample) const {
-    	throw NoriException("MicrofacetBRDF::sample(): not implemented!");
+    	//throw NoriException("MicrofacetBRDF::sample(): not implemented!");
 
         // Note: Once you have implemented the part that computes the scattered
         // direction, the last part of this function should simply return the
         // BRDF value divided by the solid angle density and multiplied by the
         // cosine factor from the reflection equation, i.e.
         // return eval(bRec) * Frame::cosTheta(bRec.wo) / pdf(bRec);
+        if (Frame::cosTheta(bRec.wi) <= 0) {
+            return Color3f(0.0f);
+        }
+        if (_sample.x() > m_ks) {  //漫反射
+            Point2f sample((_sample.x() - m_ks) / (1.f - m_ks), _sample.y());
+            bRec.wo = Warp::squareToCosineHemisphere(sample);
+        } else {  //高光
+            Point2f sample(_sample.x() / m_ks, _sample.y());
+            Vector3f wh = Warp::squareToBeckmann(sample, m_alpha);
+            bRec.wo = ((2.0f * wh.dot(bRec.wi) * wh) - bRec.wi).normalized();
+        }
+        if (bRec.wo.z() < 0.f) {
+            return Color3f(0.0f);
+        }
+        // Note: Once you have implemented the part that computes the scattered
+        // direction, the last part of this function should simply return the
+        // BRDF value divided by the solid angle density and multiplied by the
+        // cosine factor from the reflection equation, i.e.
+        // 应该返回 BRDF 计算结果除以立体角密度并乘以反射系数（注释提示的，我也不知道为啥）
+        return eval(bRec) * Frame::cosTheta(bRec.wo) / pdf(bRec);
     }
 
     bool isDiffuse() const {
@@ -62,6 +97,22 @@ public:
            handled by sampling techniques for diffuse/non-specular materials,
            hence we return true here */
         return true;
+    }
+    static float DistributeBeckmann(const Vector3f& wh, float alpha) {//Beckmann法线分布项
+        float tanTheta = Frame::tanTheta(wh);
+        float cosTheta = Frame::cosTheta(wh);
+        float a = std::exp(-(tanTheta * tanTheta) / (alpha * alpha));
+        float b = M_PI * alpha * alpha * std::pow(cosTheta, 4.0f);
+        return a / b;
+    }
+
+    static float G1(const Vector3f& wv, const Vector3f& wh, float alpha) {//Beckmann几何遮蔽项
+        float c = wv.dot(wh) / Frame::cosTheta(wv);
+        if (c <= 0) {
+        return 0;
+        }
+        float b = 1.0f / (alpha * Frame::tanTheta(wv));
+        return b < 1.6f ? (3.535f * b + 2.181f * b * b) / (1.f + 2.276f * b + 2.577f * b * b) : 1;
     }
 
     std::string toString() const {
